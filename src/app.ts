89dfb7cdb9917b7de9b328e01c08fac709765091
ahmadparvej <ahmadparvej58@gmail.com>
2024-11-26 21:50:39 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { HttpError } from "http-errors";
+import logger from "./config/logger";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.use((err: HttpError, req: Request, res: Response) => {
+  logger.error(err.message);
+
+  res.status(err.statusCode).json({
+    errors: [
+      {
+        message: err.message,
+        type: err.name,
+      },
+    ],
+  });
 });
 
 export default app;
