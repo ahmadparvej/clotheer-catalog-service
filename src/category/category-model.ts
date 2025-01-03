77eb266133510c404a4124d771f0561d5e96aa04
ahmadparvej <ahmadparvej58@gmail.com>
2025-01-03 20:39:28 +0530

    export category schema

--- src/category/category-model.ts
+++ src/category/category-model.ts
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 interface PriceConfiguration {
   [key: string]: {
     priceType: "base" | "additional";
@@ -17,3 +19,53 @@ export interface Category {
   priceConfiguration: PriceConfiguration;
   attributes: Attribute[];
 }
+
+const priceConfigurationSchema = new mongoose.Schema<PriceConfiguration>({
+  priceType: {
+    type: String,
+    enum: ["base", "additional"],
+    required: true,
+  },
+  availableOptions: {
+    type: [String],
+    required: true,
+  },
+});
+
+const attributeSchema = new mongoose.Schema<Attribute>({
+  name: {
+    type: String,
+    required: true,
+  },
+  widgetType: {
+    type: String,
+    enum: ["radio", "switch"],
+    required: true,
+  },
+  defaultValue: {
+    type: String,
+    required: true,
+  },
+  availableOption: {
+    type: [String],
+    required: true,
+  },
+});
+
+const categorySChema = new mongoose.Schema<Category>({
+  name: {
+    type: String,
+    required: true,
+  },
+  priceConfiguration: {
+    type: Map,
+    of: priceConfigurationSchema,
+    required: true,
+  },
+  attributes: {
+    type: [attributeSchema],
+    required: true,
+  },
+});
+
+export const Category = mongoose.model("Category", categorySChema);
